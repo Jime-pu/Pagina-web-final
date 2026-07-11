fd75c6129f516a803b49f2239bc94a738357a486
Jime-pu <a20241018@pucp.edu.pe>
2026-07-10 19:58:40 -0500

    Update PC3.py

--- PC3.py
+++ PC3.py
@@ -94,7 +94,7 @@ elif opciones == 'Que tanto sabes de los chinos?':
     
     # Poner el enlace (esto es HTML, pero escrito desde Python) Con esto podemos escribiri HTML en phyton para que el amongus te mande a un video
     st.markdown(
-        f'<a href="https://www.youtube.com/watch?v=VoQ1q41iJS0" target="_blank">'
+        f'<a href="https://youtu.be/wDZxDBDRync?si=Hzk4bsb29-xAUfTS">'
         f'<img src="data:image/png;base64,{img_data}" width="50">'
         f'<p style="text-align:center;font-size:12px;color:gray;">Causa</p>'
         f'</a>',
